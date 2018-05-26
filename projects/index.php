250ee4c3603964bffa5fdb4769da5c3c927d09ed
id523 <edward.giles@dulwich-suzhou.cn>
2018-05-26 17:18:40 +0800

    Fixed redirect in projects

--- projects/index.php
+++ projects/index.php
@@ -2,7 +2,7 @@
 $redir = empty($_SERVER['SERVER_HTTPS']) ? 'http://' : 'https://';
 $redir .= $_SERVER['SERVER_NAME'];
 $redir .= dirname(dirname($_SERVER['PHP_SELF']));
-$redir .= '/?p=jsonindex';
+$redir .= '/?p=index';
 header("Location: $redir");
 exit();
 ?>
